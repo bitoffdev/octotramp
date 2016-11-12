43641b07971fa2b67c433dc30a58ca00a011b6c7
ejmsoftware <emoney543@gmail.com>
2016-11-12 18:59:56 -0500

    Fix Graphics Buffer Hardware Issue

--- environment.js
+++ environment.js
@@ -1,3 +1,24 @@
+// Non-buffered version
+function Environment(width, height){
+	this.trampolines = [];
+
+	this.scrollX = 0;
+
+	this.addTrampoline = function(x){
+		this.trampolines.push(x);
+	}
+
+	this.drawEnvironment = function(){
+		background(220,220,220);
+		noStroke();
+		fill(color(0, 255, 0));
+		for (var i=0;i<this.trampolines.length;i++){
+			ellipse(this.trampolines[i]-this.scrollX, height-20, 100, 40);
+		}
+	}
+}
+// Buffered Version
+/*
 function Environment(width, height){
 	// load sprites
 	img = loadImage("assets/trampoline.png");
@@ -26,24 +47,4 @@ function Environment(width, height){
 		image(this.buffer,-this.scrollX,0);
 	}
 }
-
-function EnvironmentFrame(width, height){
-	// create buffer
-	this.buffer = createGraphics(width, height);
-	this.buffer.background(220,220,220);
-
-	this.trampolines = [];
-
-	for (var i=0;i<30;i++){
-		this.buffer.noStroke();
-		this.buffer.fill(color(156, 218, 239));
-		this.buffer.ellipse(i*200, height-20, 100, 40);
-	}
-	this.buffer.rect(30*200, 0, 20, height);
-
-	this.scrollX = 0;
-
-	this.drawEnvironment = function(){
-		image(this.buffer,-this.scrollX,0);
-	}
-}
+*/
