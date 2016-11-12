24883b11a22a45e256b4f74213f9c4152c43c72a
cjb5326 <cjb5326@cs.rit.edu>
2016-11-12 16:52:38 -0500

    Merge

--- sketch.js
+++ sketch.js
@@ -39,12 +39,9 @@ function setup()
 {
   // set canvas size
   createCanvas(GAME_WIDTH,GAME_HEIGHT);
-<<<<<<< HEAD
 	characterImage = loadImage("assets/octocat.png");
 	image(characterImage, 0, 0);
-=======
 	environment = new Environment(5000,GAME_HEIGHT);
->>>>>>> 37711129b59bed8a1185989a8b42add36803aced
 }
 
 function drawBackground(){
@@ -86,7 +83,7 @@ function drawPlayer(){
 function draw()
 {
 	environment.drawit();
-	//drawBackground();
+	drawBackground();
 	drawPlayer();
 }
 
