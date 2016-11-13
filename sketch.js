bfd01addac0983c50777e33d6f4d4e6f7127926e
MAXTOP\Maximillian <mrm8391@rit.edu>
2016-11-13 17:27:35 -0500

    Remove obnoxious carots in merge

--- sketch.js
+++ sketch.js
@@ -10,14 +10,25 @@ const GAME_WIDTH=640;
 const GAME_HEIGHT=480;
 
 const DIST_BETWEEN_TRAMPS=100;
+const GAME_TITLE_YPOS = ((GAME_HEIGHT/2)/2);
+const GAME_TITLE = "OCTOTRAMP";
+const GAME_TITLE_SIZE = 32;
+const START_MESSAGE = "press any key to start";
+const START_MESSAGE_YPOS = ((GAME_HEIGHT/2)/2) + (((GAME_HEIGHT/2)/2)/2);
+const START_MESSAGE_SIZE = 24;
 
 var DEFAULT_SPEED=10;
+var STARTING_SPEED = 0;
+
+var GAME_STARTED = 0;
+
+var characterImage;
 
 var nextSpot=0;
 
 class Player{
 	constructor(){
-		this.playerSpeed=DEFAULT_SPEED;
+		this.playerSpeed=STARTING_SPEED;
 		this.xpos=0;
 		this.ypos=0;
 	}
@@ -32,17 +43,34 @@ class Trampoline{
 
 
 var thePlayer=new Player();
+var environment;
+
 
 
 function setup()
 {
 	// set canvas size
 	createCanvas(GAME_WIDTH,GAME_HEIGHT);
+  // set canvas size
+  createCanvas(GAME_WIDTH,GAME_HEIGHT);
+	characterImage = loadImage("assets/octocat.png");
+	image(characterImage, 0, 0);
+	environment = new Environment(10000,GAME_HEIGHT);
 }
 
 function drawBackground(){
-	//placeholder background; just a clear white color
-	background(255);
+	// Draw Environment
+	environment.drawEnvironment();
+
+	// Draw Chris' Stuff
+	text(START_MESSAGE, GAME_WIDTH/2, START_MESSAGE_YPOS);
+	textSize(GAME_TITLE_SIZE);
+	textAlign(CENTER);
+
+	text(GAME_TITLE, GAME_WIDTH/2, GAME_TITLE_YPOS);
+	textStyle(BOLD);
+	textSize(START_MESSAGE_SIZE);
+	textAlign(CENTER);
 }
 
 /**
@@ -70,9 +98,12 @@ function drawTrampoline(){
 }
 
 function drawPlayer(){
-	thePlayer.xpos+=thePlayer.playerSpeed;
+	//thePlayer.xpos+=thePlayer.playerSpeed;
+	environment.scrollX += thePlayer.playerSpeed;
 
 	var sinShenanigans=Math.abs(sin(millis()/TIME_CONSTANT))*(GAME_HEIGHT);
+	var sinShenanigans=(sin(millis()/TIME_CONSTANT))*(GAME_HEIGHT-100);
+	if(sinShenanigans<0) sinShenanigans*=-1;
 	thePlayer.ypos=GAME_HEIGHT-sinShenanigans;
 
 	//ball is hitting the ground
@@ -90,8 +121,8 @@ function drawPlayer(){
 
 
 	// placeholder for player sprite
-	fill(255, 0, 0);
-	ellipse(thePlayer.xpos, thePlayer.ypos, 25, 25);
+	//fill(255, 0, 0);
+	image(characterImage,thePlayer.xpos, thePlayer.ypos-100, 75, 75);
 
 	// debug info
 	 fill(0);
@@ -116,12 +147,26 @@ Handle keyboard input.
 Left arrow decreases speed, right arrow increases speed
 */
 function keyPressed(){
+	if(GAME_STARTED == 0){
+		thePlayer.playerSpeed = DEFAULT_SPEED;
+		text(START_MESSAGE, GAME_WIDTH/2, START_MESSAGE_YPOS);
+		textSize(GAME_TITLE_SIZE);
+		textAlign(CENTER);
+		fill(220,220,220);
+		GAME_STARTED = 1;
+	}
 	switch(keyCode){
 		case LEFT_ARROW:
 			thePlayer.xpos-=DIST_BETWEEN_TRAMPS;
 			break;
 		case RIGHT_ARROW:
 			thePlayer.xpos+=DIST_BETWEEN_TRAMPS;
+				if(thePlayer.playerSpeed > 0){
+					thePlayer.playerSpeed-=SPEED_MODIFIER;
+				}
+			break;
+		case RIGHT_ARROW:
+				thePlayer.playerSpeed+=SPEED_MODIFIER;
 			break;
 		default:
 			break;
